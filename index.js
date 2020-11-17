e3647de50845d6e9d45398545c226269315c130b
Jueun Jeon <jjeon02@risd.edu>
2020-11-17 16:30:21 +0900

    asdf

---
+++ index.js
@@ -0,0 +1,8 @@
+function navDrop() {
+    var x = document.getElementById("myNav");
+    if (x.className === "nav") {
+      x.className += " responsive";
+    } else {
+      x.className = "nav";
+    }
+  }
