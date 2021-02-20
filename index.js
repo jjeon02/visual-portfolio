dac2de287776563e6cc3b33b1279bd5bd71c76f1
Jueun Jeon <jjeon02@risd.edu>
2021-02-20 10:58:06 +0900

    helloworld

--- index.js
+++ index.js
@@ -1,8 +1,32 @@
-function navDrop() {
-    var x = document.getElementById("myNav");
-    if (x.className === "nav") {
-      x.className += " responsive";
-    } else {
-      x.className = "nav";
+// NAVBAR
+    // NAVBAR - DEFINING VARIABLE
+    const navToggler = document.querySelector('.navbutton');
+    const navMenu = document.querySelector('.navbar ul');
+    const navLinks = document.querySelectorAll('.navbar a');
+
+    //FUNCTION - ALL EVENT LISTENER
+    allEventListners();
+
+    function allEventListners() {
+    navToggler.addEventListener('click', togglerClick);
+    navLinks.forEach( elem => elem.addEventListener('click', navLinkClick));
     }
-  }
+
+    // TOGGLE CLICK
+    function togglerClick() {
+    navToggler.classList.toggle('toggler-open');
+    navMenu.classList.toggle('open');
+    }
+
+    // NAVLINKCLICK
+    function navLinkClick() {
+        if(navMenu.classList.contains('open')){
+            navToggler.click();
+        }
+    }
+
+// GOBACK BUTTON
+    function goBack() {
+        window.history.back();
+    }
+
